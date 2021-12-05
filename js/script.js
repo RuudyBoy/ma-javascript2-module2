e141ae29114c37ae6db87572e2aafc379f6880b1
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2021-12-05 18:05:29 +0100

    1

--- js/script.js
+++ js/script.js
@@ -7,7 +7,18 @@ games.forEach(function (game) {
     container.innerHTML += `<li> <span> ${game.title} Rating: ${game.isbn} <i class="fa fa-trash" data-item="${game}"></span></li>`;
 });
 
-const trashCans = document.querySelectorAll("li span i");
+const items = document.querySelectorAll("li span");
+
+items.forEach(function (item) {
+    item.addEventListener("click", handleClick);
+});
+
+function handleClick() {
+    event.target.classList.toggle("complete");
+}
+
+
+const trashCans = document.querySelectorAll("li i");
 
     trashCans.forEach(function (can) {
         can.addEventListener("click", removeFromList);
